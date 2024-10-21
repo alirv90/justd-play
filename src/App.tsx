8e602d6d96e2cc5385354cb588bf465fc00dd6cd
alirv90 <ali.rv90@gmail.com>
2024-10-21 12:42:38 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FileExplorer from "./FileExplorer";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <FileExplorer />
+    </div>
+  );
+}
+
+export default App;
