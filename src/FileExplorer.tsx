4f22b5ceb7ae3824a2b43dd6b41708c0aa32b6be
alirv90 <ali.rv90@gmail.com>
2024-10-22 19:35:22 +0700

    improve FileExplorer

--- src/FileExplorer.tsx
+++ src/FileExplorer.tsx
@@ -1,53 +1,167 @@
-import * as React from "react";
-
 import {
   Collection,
   UNSTABLE_Virtualizer as Virtualizer,
   UNSTABLE_ListLayout as ListLayout,
+  UNSTABLE_Tree as Tree,
+  UNSTABLE_TreeItem as TreeItem,
+  UNSTABLE_TreeItemContent as TreeItemContent,
+  TreeItemProps,
+  TreeProps,
+  Button,
+  Selection,
+  Key,
 } from "react-aria-components";
-import { Tree, TreeItem } from "./components/ui/tree";
+import { tv } from "tailwind-variants";
 
-export default function FileExplorer() {
-  const renderItem = (item: FileNode): React.ReactNode => {
-    return (
-      <TreeItem key={item.id} textValue={item.title}>
-        <TreeItem.Content>
-          {item.children.length > 0 && <TreeItem.Indicator />}
-          <TreeItem.Checkbox />
-          <TreeItem.Label>{item.title}</TreeItem.Label>
-        </TreeItem.Content>
-        {item.children.length > 0 && (
-          <Collection items={item.children}>{renderItem}</Collection>
-        )}
-      </TreeItem>
-    );
-  };
+import { Checkbox } from "./components/ui/checkbox";
+import { cr } from "./components/ui/primitive";
+import { useState, useEffect, useRef } from "react";
+import { IconChevronRight } from "justd-icons";
 
-  const [layout] = React.useState(() => {
-    const layout = new ListLayout({
-      rowHeight: 35,
-    });
-    return layout;
-  });
+const Indicator = () => {
   return (
-    <Virtualizer layout={layout}>
-      <Tree
-        className="w-full max-w-xl h-80"
-        aria-label="Files"
-        selectionMode="multiple"
-        items={files}
-      >
-        {files.map(renderItem)}
-      </Tree>
-    </Virtualizer>
+    <Button className="shrink-0 relative" slot="chevron">
+      <>
+        <IconChevronRight className="size-5" />
+      </>
+    </Button>
   );
-}
+};
+
+const ItemCheckbox = () => {
+  return <Checkbox slot="selection" />;
+};
+
+const ItemLabel = (props: React.HtmlHTMLAttributes<HTMLSpanElement>) => {
+  return <span {...props} />;
+};
 
 type FileNode = {
   id: number;
   title: string;
   children: FileNode[];
 };
+
+const itemStyles = tv({
+  base: [
+    "[&_[data-expanded]_[slot=chevron]_[data-slot=icon]]:rotate-90 outline-none [--padding:20px] p-[0.286rem_0.286rem_0.286rem_0.571rem] pl-[calc((var(--tree-item-level)-1)*20px+0.571rem+var(--padding))]",
+    "[&_[slot=chevron]]:outline-none [&_[slot=chevron]_[data-slot=icon]]:text-muted-fg",
+    "data-[has-child-rows]:[--padding:0px]",
+  ],
+  variants: {
+    isExpanded: {
+      true: "[&_[slot=chevron]_[data-slot=icon]]:text-fg [&_[slot=chevron]_[data-slot=icon]]:rotate-90 [&_[slot=chevron]_[data-slot=icon]]:transition [&_[slot=chevron]_[data-slot=icon]]:duration-200",
+    },
+    isFocusVisible: {
+      true: "[&_[slot=chevron]_[data-slot=icon]]:text-fg focus:outline-none focus-visible:ring-1 focus-visible:ring-primary",
+    },
+    isDisabled: {
+      true: "opacity-50 forced-colors:text-[GrayText]",
+    },
+  },
+});
+
+const FileTreeItem = (props: TreeItemProps<FileNode>) => {
+  const childItems = props.value?.children;
+  const itemRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (props.id !== 23) {
+      return;
+    }
+
+    console.log("focusing");
+  }, [props.id]);
+
+  return (
+    <>
+      <TreeItem
+        className={cr(props.className, (className, renderProps) =>
+          itemStyles({
+            ...renderProps,
+            className,
+          })
+        )}
+        ref={itemRef}
+        {...props}
+      >
+        <TreeItemContent>
+          {({ hasChildRows }) => (
+            <div className="flex items-center">
+              {hasChildRows && <Indicator />}
+              <ItemCheckbox />
+              <ItemLabel>{props.children}</ItemLabel>
+            </div>
+          )}
+        </TreeItemContent>
+        <Collection items={childItems}>
+          {(item: FileNode) => (
+            <FileTreeItem id={item.id} textValue={item.title}>
+              {item.title}
+            </FileTreeItem>
+          )}
+        </Collection>
+      </TreeItem>
+    </>
+  );
+};
+
+const treeStyles = tv({
+  base: "flex border max-h-96 min-w-72 [&::-webkit-scrollbar]:size-0.5 [scrollbar-width:thin] py-2 rounded-lg bg-bg cursor-default lg:text-sm flex-col overflow-auto forced-color-adjust-none outline-none",
+  variants: {
+    isFocusVisible: {
+      true: "outline-offset-[-1px] outline-2 outline-primary",
+    },
+  },
+});
+
+export default function FileExplorer(props: TreeProps<unknown>) {
+  const [layout] = useState(() => {
+    return new ListLayout({
+      rowHeight: 35,
+    });
+  });
+  const [selection, setSelection] = useState<Selection>(new Set([23]));
+  const [expandedKeys, setExpandedKeys] = useState<Set<Key>>();
+  const treeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    //treeRef.current?.focus();
+  }, []);
+
+  return (
+    <Virtualizer layout={layout}>
+      <Tree
+        ref={treeRef}
+        className={cr(props.className, (className, renderProps) =>
+          treeStyles({
+            ...renderProps,
+            className,
+          })
+        )}
+        {...props}
+        selectionMode="single"
+        onSelectionChange={(sel) => {
+          setSelection(sel);
+        }}
+        selectedKeys={selection}
+        defaultExpandedKeys={[1, 2, 3, 4, 21, 22, 23]}
+        expandedKeys={expandedKeys}
+        onExpandedChange={(keys) => {
+          setExpandedKeys(keys);
+        }}
+        aria-label="File Explorer"
+        items={files}
+      >
+        {(item: FileNode) => (
+          <FileTreeItem id={item.id} textValue={item.title}>
+            {item.title}
+          </FileTreeItem>
+        )}
+      </Tree>
+    </Virtualizer>
+  );
+}
 
 const files: FileNode[] = [
   {
@@ -66,8 +180,11 @@ const files: FileNode[] = [
                 id: 4,
                 title: "Q1 Report",
                 children: [
-                  { id: 5, title: "Summary", children: [] },
-                  { id: 6, title: "Details", children: [] },
+                  ...Array.from({ length: 100 }, (_, index) => ({
+                    id: 1000 + index,
+                    title: `Item ${index + 1}`,
+                    children: [],
+                  })),
                 ],
               },
             ],
